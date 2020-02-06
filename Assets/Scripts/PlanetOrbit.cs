11c984545eebbece15eefc9b745c28b61deadc01
Jakobi Talbot <jakobi.talbot@gmail.com>
2020-02-06 20:27:23 +1100

    - Initial commit

---
+++ Assets/Scripts/PlanetOrbit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetOrbit : MonoBehaviour
+{
+    [SerializeField]
+    private float m_orbitPeriod = 24;
+    [SerializeField]
+    private float m_speedCoefficient = 1;
+
+    private Transform m_transform;
+
+    void Awake()
+    {
+        m_transform = transform;
+    }
+
+    void Update()
+    {
+        m_transform.Rotate(new Vector3(0, 360f) / 3600f / m_orbitPeriod * m_speedCoefficient * Time.deltaTime);
+    }
+}
