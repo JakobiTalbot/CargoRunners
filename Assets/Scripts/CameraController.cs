2abd459d3b0a884e9db2990c43c2d152611b0579
Jakobi Talbot <jakobi.talbot@gmail.com>
2020-02-06 20:57:58 +1100

    - Camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Pan")]
+    [SerializeField]
+    private float m_panSpeedCoefficient = 1f;
+    [Header("Zoom")]
+    [SerializeField]
+    private float m_zoomSpeedCoefficient = 1f;
+    [SerializeField]
+    private float m_minCameraYZoom = 50f;
+    [SerializeField]
+    private float m_maxCameraYZoom = 500f;
+
+    private Transform m_transform;
+
+    private Vector3 m_lastPos;
+
+    private Vector3 m_panDiff;
+    private Vector3 m_zoomDiff;
+
+    void Awake()
     {
-        
+        m_transform = transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        #if UNITY_EDITOR
+            MouseInput();
+        #endif
+
+        m_transform.position += m_panDiff;
+        if (!(m_transform.position.y + m_zoomDiff.y < m_minCameraYZoom)
+            && !(m_transform.position.y + m_zoomDiff.y > m_maxCameraYZoom))
+        {
+            m_transform.position += m_zoomDiff;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        m_panDiff *= 0.9f;
+        m_zoomDiff *= 0.9f;
+    }
+
+    private void MouseInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            m_lastPos = Input.mousePosition;
+            m_zoomDiff = Vector3.zero;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            m_panDiff = new Vector3(m_lastPos.x - Input.mousePosition.x, 0, m_lastPos.y - Input.mousePosition.y) * m_transform.position.y * m_panSpeedCoefficient * Time.deltaTime;
+            m_lastPos = Input.mousePosition;
+        }
+
+        // zoom
+        m_zoomDiff += m_transform.forward * Input.mouseScrollDelta.y * m_zoomSpeedCoefficient * Time.deltaTime;
     }
 }
